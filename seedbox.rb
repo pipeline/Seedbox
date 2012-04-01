4eff723c343db80bfb268844317acc38e65947ab
Benjamin Kearns <benjamin.kearns@gmail.com>
2012-04-01 19:01:21 +1200

    Fix for initial setting of password

--- seedbox.rb
+++ seedbox.rb
@@ -23,7 +23,7 @@ before do
     @user = User.get(session[:user_id])
   end
   
-  if @user != nil && (@user.credit_expires == nil || @user.credit_expires < Date.today) && @user.admin != true && path != '/users/credit_expired' && path != '/users/checkout' && path != '/users/confirm_payment'
+  if @user != nil && (@user.credit_expires == nil || @user.credit_expires < Date.today) && @user.admin != true && path != '/users/credit_expired' && path != '/users/checkout' && path != '/users/confirm_payment' && path != '/user/set_password' && path != '/users/change_password'
     redirect '/users/credit_expired'
   end
 end
